c94d92a4235088bb33bc867867f8762e9b3fead9
Öykü Örs <oyku@Oykus-MacBook-Pro.local>
2021-07-28 01:50:01 +0300

    Worked on dashboard page.

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,6 +1,14 @@
 import React from 'react'
+import './styles.scss'
 
 const Dashboard = () => {
-  return <div></div>
+  return <React.Fragment>
+    <header className="header">
+      <h1 className="heading-primary">
+        <span className="heading-primary-main">Ceomed</span>
+        <span className="heading-primary-sub">Ticket System</span>
+      </h1>
+    </header>
+  </React.Fragment>
 }
 export default Dashboard;
