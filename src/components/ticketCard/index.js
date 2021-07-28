86759194ee1bdcef2b9a1456bf3e67094ed58a18
öykü <oyku_1224@hotmail.com>
2021-07-28 13:57:14 +0300

    avatar added to ticket card

--- src/components/ticketCard/index.js
+++ src/components/ticketCard/index.js
@@ -1,85 +1,4 @@
-// import React, { useEffect } from 'react';
-// import './styles.scss'
-// import Card from '@material-ui/core/Card';
-// import CardActionArea from '@material-ui/core/CardActionArea';
-// import CardActions from '@material-ui/core/CardActions';
-// import CardContent from '@material-ui/core/CardContent';
-// import Button from '@material-ui/core/Button';
-// import Typography from '@material-ui/core/Typography';
-
-// import { connect } from 'react-redux';
-// import { bindActionCreators } from "redux";
-// import * as ticketActions from '../../redux/actions/ticketActions'
-
-// // const useStyles = makeStyles({
-// //   root: {
-// //     maxWidth: 345,
-// //   },
-// //   media: {
-// //     height: 140,
-// //   },
-// // });
-
-// const TicketCard = (props) => {
-//   useEffect(()=>{
-//         props.actions.getTickets()
-//   },[])
- 
-//   const selectedTicket = ticket => {
-//     console.log(ticket.id)
-//     props.actions.getTicket(ticket.id)
-//   }
- 
-//   return (
-//     <React.Fragment>
-//       {props.tickets.map(ticket => (
-//           <Card key={ticket.id} className="cards" onClick={()=>selectedTicket(ticket)} >
-//           <CardActionArea>
-//             <CardContent>
-//               <Typography gutterBottom variant="body2" component="p" className="date">
-//                   {ticket.date}  
-//               </Typography>
-//               <Typography variant="body2" color="textSecondary" component="p">
-//                 {ticket.message}
-//               </Typography>
-//             </CardContent>
-//           </CardActionArea>
-//           <CardActions>
-//             <Button size="small" color="primary">
-//               delete
-//             </Button>
-//             <Button size="small" color="primary">
-//               Read More
-//             </Button>
-//           </CardActions>
-//         </Card>
-
-//       ))}
-     
-//     </React.Fragment>
-   
-
-    
-//   );
-// }
-// function mapStateToProps(state){
-//   return {
-//     tickets:state.getTicketsReducer,
-//     ticket:state.getTicketReducer
-//   }
-// }
-
-// function mapDispatchToProps(dispatch) {
-//   return {
-//     actions: {
-//       getTickets: bindActionCreators(ticketActions.getTickets, dispatch),
-//       getTicket: bindActionCreators(ticketActions.getTicket,dispatch)
-//     }
-//   }
-// }
-// export default connect(mapStateToProps,mapDispatchToProps)(TicketCard);
-
-import React,{useEffect} from 'react';
+import React, { useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Typography from '@material-ui/core/Typography';
@@ -90,6 +9,8 @@ import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import ListItemText from '@material-ui/core/ListItemText';
 import ListSubheader from '@material-ui/core/ListSubheader';
 import Avatar from '@material-ui/core/Avatar';
+import { green } from '@material-ui/core/colors';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 
 
 import { connect } from 'react-redux';
@@ -104,13 +25,13 @@ const useStyles = makeStyles((theme) => ({
     padding: theme.spacing(2, 2, 0),
   },
   containerTicket: {
-    backgroundColor:'pink !important'
+    backgroundColor: 'pink !important'
 
   },
   paper: {
     paddingBottom: 50,
     width: '75vh',
-    margin:"20px 60px 20px 60px"
+    margin: "20px 60px 20px 60px"
   },
   list: {
     marginBottom: theme.spacing(2),
@@ -125,6 +46,10 @@ const useStyles = makeStyles((theme) => ({
   grow: {
     flexGrow: 1,
   },
+  green:{
+    color: '#fff',
+    backgroundColor: green[500],
+  },
   fabButton: {
     position: 'absolute',
     zIndex: 1,
@@ -138,14 +63,14 @@ const useStyles = makeStyles((theme) => ({
 const TicketCard = (props) => {
   const classes = useStyles();
 
-  useEffect(()=>{
-            props.actions.getTickets()
-      },[])
-     
-      const selectedTicket = ticket => {
-        console.log(ticket.id)
-        props.actions.getTicket(ticket.id)
-      }
+  useEffect(() => {
+    props.actions.getTickets()
+  }, [])
+
+  const selectedTicket = ticket => {
+    console.log(ticket.id)
+    props.actions.getTicket(ticket.id)
+  }
 
   return (
     <React.Fragment>
@@ -155,18 +80,20 @@ const TicketCard = (props) => {
           Tickets
         </Typography>
         <List className={classes.list}>
-        {props.tickets.map(ticket => (
-               <div key={ticket.id} onClick={()=>selectedTicket(ticket)} >
-               <ListSubheader className={classes.subheader}>{ticket.date}</ListSubheader>
-               <ListItem button>
-                 <ListItemAvatar>
-                   <Avatar alt="Profile Picture" src={ticket.requester} />
-                 </ListItemAvatar>
-                 <ListItemText primary={ticket.subject} secondary={ticket.message} />
-               </ListItem>
-             </div>
-      
-       
+          {props.tickets.map(ticket => (
+            <div key={ticket.id} onClick={() => selectedTicket(ticket)} >
+              <ListSubheader className={classes.subheader}>{ticket.date}</ListSubheader>
+              <ListItem button>
+                <ListItemAvatar>
+                  <Avatar className={classes.green}>
+                    <AccountCircleIcon />
+                  </Avatar>
+                </ListItemAvatar>
+                <ListItemText primary={ticket.subject} secondary={ticket.message} />
+              </ListItem>
+            </div>
+
+
           ))}
         </List>
       </Paper>
@@ -174,10 +101,10 @@ const TicketCard = (props) => {
   );
 }
 
-function mapStateToProps(state){
+function mapStateToProps(state) {
   return {
-    tickets:state.getTicketsReducer,
-    ticket:state.getTicketReducer
+    tickets: state.getTicketsReducer,
+    ticket: state.getTicketReducer
   }
 }
 
@@ -185,9 +112,9 @@ function mapDispatchToProps(dispatch) {
   return {
     actions: {
       getTickets: bindActionCreators(ticketActions.getTickets, dispatch),
-      getTicket: bindActionCreators(ticketActions.getTicket,dispatch)
+      getTicket: bindActionCreators(ticketActions.getTicket, dispatch)
     }
   }
 }
-export default connect(mapStateToProps,mapDispatchToProps)(TicketCard);
+export default connect(mapStateToProps, mapDispatchToProps)(TicketCard);
 
